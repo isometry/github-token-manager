bf6c693c1ffff958b2c3bc4b746e9ef351ec13b1
Robin Breathe <robin@isometry.net>
2024-04-07 02:43:47 +0200

    refactor: simplify resource naming

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -75,17 +75,17 @@ var _ = Describe("controller", Ordered, func() {
 			cmd = exec.Command("make", "install")
 			_, err = utils.Run(cmd)
 
-			By("deploying the controller-manager")
+			By("deploying the manager")
 			cmd = exec.Command("make", "deploy", fmt.Sprintf("IMG=%s", projectimage))
 			_, err = utils.Run(cmd)
 			ExpectWithOffset(1, err).NotTo(HaveOccurred())
 
-			By("validating that the controller-manager pod is running as expected")
+			By("validating that the manager pod is running as expected")
 			verifyControllerUp := func() error {
 				// Get pod name
 
 				cmd = exec.Command("kubectl", "get",
-					"pods", "-l", "control-plane=controller-manager",
+					"pods", "-l", "control-plane=manager",
 					"-o", "go-template={{ range .items }}"+
 						"{{ if not .metadata.deletionTimestamp }}"+
 						"{{ .metadata.name }}"+
@@ -100,7 +100,7 @@ var _ = Describe("controller", Ordered, func() {
 					return fmt.Errorf("expect 1 controller pods running, but got %d", len(podNames))
 				}
 				controllerPodName = podNames[0]
-				ExpectWithOffset(2, controllerPodName).Should(ContainSubstring("controller-manager"))
+				ExpectWithOffset(2, controllerPodName).Should(ContainSubstring("manager"))
 
 				// Validate pod status
 				cmd = exec.Command("kubectl", "get",
